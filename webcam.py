9669717b2f8db46dc40bb8793f92e1246298aae4
ashish-garg18 <gashish641@gmail.com>
2020-10-09 11:55:57 +0530

    Added webcam file, Capture image through webcam

---
+++ webcam.py
@@ -0,0 +1,14 @@
+# install package pip install opencv-python
+
+import cv2
+
+imgCapture = cv2.VideoCapture(0)
+result = True
+
+while(result):
+    ret, frame = imgCapture.read()
+    cv2.imwrite("test.jpg", frame)
+    result = False
+    print("Image Captured.. ")
+
+imgCapture.release()
